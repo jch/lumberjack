f25440ea04877766fdd4b2a439d45da88ae51b13
Jerry Cheung <jch@whatcodecraves.com>
2011-01-19 13:21:46 -0800

    testing pre-commit

--- html/test.rb
+++ html/test.rb
@@ -8,21 +8,43 @@ Capybara.register_driver :selenium do |app|
   Capybara::Driver::Selenium.new(app, :browser => :chrome)
 end
 
-@b = Capybara::Session.new(:selenium)
-@results = {}
+module Lumberjack
+  class AcceptanceTests
+    class<<self
+      def run
+        @b = Capybara::Session.new(:selenium)
+        @results = {}
+        @success = true
 
-def run_test(name, t = nil)
-  @b.visit "/"
-  @b.visit("/test.html##{name.to_s}")
-  sleep t if t
-  @results[name.to_sym] = @b.find('.result').text
+        def run_test(name, t = nil)
+          @b.visit "/"
+          @b.visit("/test.html##{name.to_s}")
+          sleep t if t
+          @results[name.to_sym] = @b.find('.result').text
+          @success = @success && @b.find('.failed').text == "0"
+          if not @success
+            puts "FAILED!!!"
+          end
+        end
+
+        puts "\nRunning Lumberjack acceptance tests...\n"
+        Benchmark.bm(15) do |x|
+          x.report("test_lumberjack") { run_test :test_lumberjack }
+          x.report("test_rails2") { run_test :test_rails2, 6 }
+          x.report("test_rails3") { run_test :test_rails3 }
+        end
+        puts
+
+
+        @results.each_pair do |name, result|
+          puts name
+          result.split("\n").each do |s|
+            puts "  " + s
+          end
+          puts
+        end
+        @success
+      end
+    end
+  end
 end
-
-Benchmark.bm(15) do |x|
-  x.report { run_test :test_lumberjack }
-  x.report { run_test :test_rails2, 3 }
-  x.report { run_test :test_rails3 }
-end
-
-puts "\n"
-pp @results
